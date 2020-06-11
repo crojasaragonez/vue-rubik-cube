026eb96827cb4edd8e414a041d3cdb5c2f182aae
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2020-06-10 23:33:23 -0600

    e2e tests added

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,51 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Cube layout", () => {
+  it("contains Fork me on GitHub image", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.get('[alt="Fork me on GitHub"]').should("be.visible");
+  });
+
+  it("contains a cube-container class", () => {
+    cy.visit("/");
+    cy.get(".container")
+      .find(".cube-container")
+      .should("exist");
+  });
+
+  it("contains a cube class", () => {
+    cy.visit("/");
+    cy.get(".container")
+      .find(".cube")
+      .should("exist");
+  });
+
+  it("has all sides", () => {
+    cy.visit("/");
+    cy.get(".container")
+      .find(".side .front")
+      .should("exist");
+    cy.get(".container")
+      .find(".side .top")
+      .should("exist");
+    cy.get(".container")
+      .find(".side .bottom")
+      .should("exist");
+    cy.get(".container")
+      .find(".side .left")
+      .should("exist");
+    cy.get(".container")
+      .find(".side .right")
+      .should("exist");
+    cy.get(".container")
+      .find(".side .back")
+      .should("exist");
+  });
+
+  it("has exactly 54 cells", () => {
+    cy.visit("/");
+    cy.get(".container")
+      .find(".cell")
+      .should("have.length", 54);
   });
 });
